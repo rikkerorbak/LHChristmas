670b21b960c0ca39f013ea2046587958f4b0ff40
rikkerorbak <rikke.rorbak@gmail.com>
2021-10-16 11:48:05 +0200

    second commit

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,17 @@
 import Phaser from 'phaser'
+import Game from './scenes/Game'
 
+const config = {
+   type: Phaser.AUTO,
+   width: 800,
+   height: 500,
+   physics: {
+      default: 'arcade',
+      arcade: {
+         gravity: {y: 0}
+      }
+   },
+   scene: [Game]
+}
+
+export default new Phaser.Game(config)
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,4 +1,14 @@
 import Phaser from "phaser";
 
 export default class Game extends Phaser.Scene {
+
+   constructor() {
+      super('game')
+   }
+
+   preload() { }
+
+   create() { }
+
+
 }
